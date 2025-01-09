b98d24ab446dd1317a2b9fcd70d7838d71e6f64c
Riccardo Abballe <riccardo.abballe@gmail.com>
2025-01-09 18:38:19 +0100

    Added button and BL for changing label name

---
+++ My first ios app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My first ios app
+//
+//  Created by Riccardo Abballe on 09/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var message: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func clickedSaveBtn(_ sender: Any) {
+        message.text = "Ciao mondo";
+        
+        print("Clicked Saved")
+    }
+}
+
