b4a74fa5b30c57ff30ebf1c2c5434643f395e5b4
Riccardo Abballe <riccardo.abballe@gmail.com>
2025-01-09 18:38:19 +0100

    Initial Commit

---
+++ My first ios app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My first ios app
+//
+//  Created by Riccardo Abballe on 09/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
