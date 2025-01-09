a5b07f9153102eb10167bd6459dd76c00a48f4f0
Riccardo Abballe <riccardo.abballe@gmail.com>
2025-01-09 19:45:38 +0100

    Changed to english

--- My first ios app/ViewController.swift
+++ My first ios app/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
 
 
     @IBAction func clickedSaveBtn(_ sender: Any) {
-        message.text = "Ciao Mondo";
+        message.text = "Hello World";
         
         print("Clicked Saved")
     }
